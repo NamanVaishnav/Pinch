0c9c8b0deef8372c175dcfe5aa7bb40250304550
Naman Vaishnav <namanmvaishnav@gmail.com>
2022-08-14 12:42:16 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Naman Vaishnav on 14/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
